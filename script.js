bee41d814f35a901b60724fe26d5af1806a3c4bd
santhoshkumar224 <160740488+santhoshkumar224@users.noreply.github.com>
2024-03-12 16:44:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-function firstChar(text) {
-  // your code here
+function firstChar(str) {
+    for (let char of str) {
+        if (char !== ' ') {
+            return char;
+        }
+    }
+    return '';
 }
 
-// Do not change the code below
-
-const text = prompt("Enter text:");
-alert(firstChar(text));
+// Test cases
+console.log(firstChar(' Rosa Parks ')); // Output: 'R'
+console.log(firstChar(' Hello World ')); // Output: 'H'
+console.log(firstChar(' ')); // Output: ''
+console.log(firstChar('')); // Output: ''
